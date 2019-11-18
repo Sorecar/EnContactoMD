dc9b9d0cf207a86b8929b27cc48481aa90d05121
FAMSA <FAMSA@LAPTOP-LNP6PGD9>
2019-11-18 09:53:16 -0600

    luis

--- src/Controlador/FXMLAdmiController.java
+++ src/Controlador/FXMLAdmiController.java
@@ -1,19 +1,37 @@
 package Controlador;
 
+import Modelo.BaseConexion;
+import Modelo.Usuario;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class FXMLAdmiController implements Initializable {
     private int Id;
+    @FXML
+    private TableColumn<Usuario, String> ColumUsuario;
+    @FXML
+    private TableColumn<Usuario, String> ColumEstado;
+    @FXML
+    private ObservableList <Usuario> data = FXCollections.observableArrayList();
 
     public int getId() {
         return Id;
@@ -29,7 +47,33 @@ public class FXMLAdmiController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
+    BaseConexion conn = new BaseConexion();
+    Connection con = conn.getConexion();
+    Statement ps = null;
+    ResultSet rs = null;
+    
+    private void DatosTabla(){
+        String sql = "SELECT * FROM flores";
+        try {
+            this.con = this.conn.getConexion();
+            ResultSet rs = this.con.createStatement().executeQuery(sql);
+            System.out.println("Si se pudo");
+            while(rs.next()){
+                this.data.add(new Usuario(rs.getString("Usuario"), rs.getString("Contraseña"), rs.getString("Telefono"), rs.getString("Mascota")));
+                
+                System.out.println("SI");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLAdmiController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.ColumUsuario.setCellValueFactory(new PropertyValueFactory<>("Usuario"));
+        //this.nombrecolumn.setCellValueFactory(new PropertyValueFactory<>("NOMBRE"));
+    }
+    
+    
+    
 
     @FXML
     private void CerrarSesion(MouseEvent event) {
