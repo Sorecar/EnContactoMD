84e13c72030fd1b1e0c9ad1380bd152b9efa1ff9
sorec <sorec@HP-Sorecar.mshome.net>
2019-11-17 20:08:22 -0600

    Otro update

--- src/encontactomd/encontacto_administrador.sql
+++ src/encontactomd/encontacto_administrador.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
--- Host: localhost    Database: encontactomd
+-- Host: localHost    Database: encontacto
 -- ------------------------------------------------------
--- Server version	8.0.17
+-- Server version	8.0.13
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,30 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `eventos`
+-- Table structure for table `administrador`
 --
 
-DROP TABLE IF EXISTS `eventos`;
+DROP TABLE IF EXISTS `administrador`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `eventos` (
-  `IdEventos` int(11) NOT NULL AUTO_INCREMENT,
-  `ConInfo` varchar(45) NOT NULL,
-  `FechaEvento` varchar(45) NOT NULL,
-  `HoraEvento` varchar(45) NOT NULL,
-  `Evento` varchar(45) NOT NULL,
-  PRIMARY KEY (`IdEventos`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `administrador` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  `Contraseña` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `eventos`
+-- Dumping data for table `administrador`
 --
 
-LOCK TABLES `eventos` WRITE;
-/*!40000 ALTER TABLE `eventos` DISABLE KEYS */;
-INSERT INTO `eventos` VALUES (1,'12345678','10/11/2019','08:00 p.m.','Fiesta en Casa de Pablo'),(2,'Pablito_mix','07/11/2019','09:00 p.m.','Fiesta en casa del Omar');
-/*!40000 ALTER TABLE `eventos` ENABLE KEYS */;
+LOCK TABLES `administrador` WRITE;
+/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
+INSERT INTO `administrador` VALUES (1,'Perro','1234'),(2,'Pendejo','1234'),(3,'Idiota','1234');
+/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-08 16:38:48
+-- Dump completed on 2019-11-17 15:42:18
--- src/encontactomd/encontacto_usuarios.sql
+++ src/encontactomd/encontacto_usuarios.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
--- Host: localhost    Database: encontactomd
+-- Host: localHost    Database: encontacto
 -- ------------------------------------------------------
--- Server version	8.0.17
+-- Server version	8.0.13
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,15 +21,16 @@
 
 DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `usuarios` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Usuario` varchar(45) NOT NULL,
-  `Contrasena` varchar(45) NOT NULL,
-  `NumTel` varchar(45) NOT NULL,
-  `NomMascota` varchar(45) NOT NULL,
+  `Usuario` varchar(50) NOT NULL,
+  `Contraseña` varchar(50) NOT NULL,
+  `Telefono` varchar(10) NOT NULL,
+  `Mascota` varchar(45) NOT NULL,
+  `Estatus` varchar(45) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +39,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Carlos','1234','12345','Pop'),(2,'Luis','1234','56789','Max'),(3,'Yisus','1234','12345','Rex'),(4,'Pablo','1234','56789','Marco'),(5,'Juan','1234','12345','Pep');
+INSERT INTO `usuarios` VALUES (1,'Carlos','1234','5517716043','firulais','Activo'),(2,'Luis','1234','2283172578','firulais','Activo'),(3,'Pablo','1234','1234567890','firulais','Activo'),(4,'Yisus','1234','1234567890','firulais','Activo'),(10,'pop','1234','1','pop','Bloqueado'),(11,'lol','1234','123445','lol','Activo');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-08 16:38:48
+-- Dump completed on 2019-11-17 15:42:18
